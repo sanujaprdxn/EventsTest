5038b69c3c8b0fd9d033b3308e651a082fe86327
Tejesh@PRDXN <tejeshs@prdxn.com>
2019-11-22 12:44:05 +0530

    modified code

--- src/test/java/com/testng/share.java
+++ src/test/java/com/testng/share.java
@@ -1,6 +1,7 @@
 package com.testng;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class share
 {
 	@Test
-	public void demoFunction()
+	public void demoFunction() throws InterruptedException
 	{
 		System.setProperty("webdriver.chrome.driver", "lib/driver/ChromeDriver/chromedriver");
 		ChromeOptions ChromeOptions = new ChromeOptions();
@@ -20,6 +21,10 @@ public class share
 		WebDriver driver = new ChromeDriver(ChromeOptions);
 		driver.get("http://prdxnstaging.com/events/");
 		System.out.println("Opening Browser Successfully");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//*[contains (text(), 'WAR ON CANCER 2016')]")).click();
+		String Actualeventtitle = driver.getTitle();
+		System.out.println("Event title is "  + Actualeventtitle);
 		driver.close();
 	}
 }
